2cd331d270ff4fc983f4aaf14fd54197319f1a6a
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-10-08 10:49:58 +0200

    second commit lol

---
+++ bin/daemonizer.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php namespace Hyn\Teamspeak\Daemon;
+
+/**
+ * Class Daemonizer
+ *
+ * @package Hyn\Teamspeak\Daemon
+ */
+class Daemonizer {
+
+    /**
+     *
+     */
+    public static function run() {
+
+    }
+}
+
+Daemonizer::run();
---
+++ src/Console/Daemon.php
@@ -0,0 +1,5 @@
+<?php namespace Hyn\Teamspeak\Daemon\Console;
+
+class Daemon {
+
+}
