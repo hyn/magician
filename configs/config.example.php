fa5d784726b00a70757092ce0d6b7eefdb624fda
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-10-08 11:21:24 +0200

    updated example config to match required connection settings

--- configs/config.example.php
+++ configs/config.example.php
@@ -1,5 +1,23 @@
 <?php
 return [
+    /**
+     * Teamspeak server configurations
+     */
+    'teamspeak' => [
+        // we allow different keys, so you can run multiple daemons at once
+        'example' => [
+            'host' => 'ts.example.org',
+            'query_port' => 10011,
+            'server_port' => 9987,
+            // server query user
+            'username' => '',
+            // server query password
+            'password' => '',
+        ],
+    ],
+    /**
+     * Notifier adapter configurations
+     */
     'hipchat' => [
         // REQUIRED: access token generated in Hipchat
         'token' => '',
