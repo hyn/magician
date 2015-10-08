30546576f911122fccf982a375bcddd8f2a10c9d
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-10-08 11:20:24 +0200

    woops connection

--- bin/daemonizer.php
+++ bin/daemonizer.php
@@ -1,6 +1,12 @@
 #!/usr/bin/php
 <?php namespace Hyn\Teamspeak\Daemon;
 
+require_once __DIR__ .'/../vendor/autoload.php';
+
+
+
+use Hyn\Teamspeak\Daemon\Console\Daemon;
+
 /**
  * Class Daemonizer
  *
@@ -9,10 +15,40 @@
 class Daemonizer {
 
     /**
+     * Sets up the daemonizer
      *
+     * @throws \Exception
      */
     public static function run() {
+        global $argv;
 
+        $config = static::config();
+
+        if(count($argv) > 1 && array_get($config, "teamspeak.{$argv[1]}")) {
+            $connection = array_get($config, "teamspeak.{$argv[1]}");
+        } elseif(count(array_get($config, 'teamspeak', [])) === 1) {
+            $connection = array_shift(array_get($config, 'teamspeak'));
+        } else {
+            throw new \Exception("No connection found for Teamspeak");
+        }
+
+        (new Daemon($connection, static::config()));
+    }
+
+    /**
+     * Loads the config from disk, if it exists
+     *
+     * @return array
+     */
+    public static function config() {
+        $path = __DIR__ . '/../configs/config.php';
+
+        if(is_file($path))
+        {
+            return require $path;
+        }
+
+        return [];
     }
 }
 
--- src/Console/Daemon.php
+++ src/Console/Daemon.php
@@ -1,5 +1,21 @@
 <?php namespace Hyn\Teamspeak\Daemon\Console;
 
+use TeamSpeak3;
+
 class Daemon {
 
+    protected $connection = [];
+
+    protected $config = [];
+
+    protected $query;
+
+    public function __construct($connection = [], $config = []) {
+
+        $this->connection = $connection;
+
+        $this->config = $config;
+
+        $this->query = TeamSpeak3::factory("serverquery://{$connection['username']}:{$connection['password']}@{$connection['host']}:{$connection['query_port']}/?server_port={$connection['server_port']}");
+    }
 }
